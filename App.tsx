c06b0062329cdff8128d1ef0152d618867f3e1e8
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2018-11-27 12:08:37 +0900

    Add initial map

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Map from './src/components/Map/Map';
+
 export default class App extends React.Component<{}, {}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Map />
       </View>
     );
   }
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { MarkerProps } from 'react-native-maps';
+
+const ImageSources = [
+  { uri: "https://i.imgur.com/sNam9iJ.jpg" },
+  { uri: "https://i.imgur.com/N7rlQYt.jpg" },
+  { uri: "https://i.imgur.com/UDrH0wm.jpg" },
+  { uri: "https://i.imgur.com/Ka8kNST.jpg" }
+];
+
+interface MapState {
+  markers: Array<MarkerProps>;
+}
+
+export default class Map extends React.Component<{}, MapState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      markers: [
+        {
+          coordinate: {
+            latitude: 45.524548,
+            longitude: -122.6749817,
+          },
+          title: "Best Place",
+          description: "This is the best place in Portland",
+          image: ImageSources[0],
+        },
+        {
+          coordinate: {
+            latitude: 45.524698,
+            longitude: -122.6655507,
+          },
+          title: "Second Best Place",
+          description: "This is the second best place in Portland",
+          image: ImageSources[1],
+        },
+        {
+          coordinate: {
+            latitude: 45.5230786,
+            longitude: -122.6701034,
+          },
+          title: "Third Best Place",
+          description: "This is the third best place in Portland",
+          image: ImageSources[2],
+        },
+        {
+          coordinate: {
+            latitude: 45.521016,
+            longitude: -122.6561917,
+          },
+          title: "Fourth Best Place",
+          description: "This is the fourth best place in Portland",
+          image: ImageSources[3],
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.container}
+          initialRegion={{
+            latitude: 45.52220671242907,
+            longitude: -122.6653281029795,
+            latitudeDelta: 0.04864195044303443,
+            longitudeDelta: 0.040142817690068,
+          }}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+})
